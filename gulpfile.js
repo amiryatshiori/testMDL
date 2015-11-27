106126d681842881c35fb21552fc99ec370e9d0e
Amiryat Shiori <shioriamiryat@gmail.com>
2015-11-27 21:45:40 +0700

    new a lot

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('siamhtml', function() {
+console.log('SiamHTML');
+});
+
+gulp.task('sass', function () {
+    // ให้คอมไพล์ .scss ทุกไฟล์ที่อยู่ในโฟลเดอร์ scss
+    return gulp.src(['scss/**/*.scss'])
+        .pipe(sass({
+         compass: true, // ใช้ Compass 
+         style: 'compressed', // เลือก output แบบ compressed
+        }))
+        .on('error', function (err) { 
+         console.log(err.message); 
+        })
+        .pipe(gulp.dest('css')) // เก็บไฟล์ css ไว้ที่โฟลเดอร์ css
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+var paths = {
+ scripts: ['app/scripts/**/*.js'],
+ html: ['app/index.html', '!app/test.html'], 
+ dist: ['dist/']
+};
+
+gulp.task('default', function(){
+ gulp.src(paths.scripts.concat(paths.html))
+ .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('default', ['siamhtml','browser-sync'], function() {
+    //gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch(['**/*.html'], browserSync.reload);
+    gulp.watch(['css/**/*.css'], browserSync.reload);
+});
